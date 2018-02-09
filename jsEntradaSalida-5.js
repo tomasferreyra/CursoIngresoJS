f2f95543519e07f02425ccae4d5814bc2eaa3ccb
Tomás Iván Ferreyra Bussolo <ferreyrabussolotomas@gmail.com>
2018-02-08 21:34:17 -0300

    ejercicio 3, 4 y 5
    document, id

---
+++ jsEntradaSalida-5.js
@@ -0,0 +1,14 @@
+/*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
+ej.: "Usted se llama José y tiene 66 años" 	*/
+function Mostar()
+{	
+	var nombre;
+	nombre=document.getElementById('elNombre').value;
+	alert(nombre);
+
+	var edad;
+	edad=document.getElementById('laEdad').value;
+	alert(edad);
+}
+
+
