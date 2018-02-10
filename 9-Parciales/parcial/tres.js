d45c1abf868418f971d59a4b976bdb85ce30983a
Tomás Iván Ferreyra Bussolo <ferreyrabussolotomas@gmail.com>
2018-02-09 22:14:39 -0300

    ejercicio 3

--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,23 @@
 function Mostrar()
 {
+	var largo;
+	var ancho;
+	var terreno;
+	var resultado;
+
+	largo=document.getElementById('alrgo').value;
+	ancho=document.getElementById('ancho').value;
+
+	largo=parseInt(largo);
+	ancho=parseInt(ancho);
+	terreno=parseInt(terreno);
+	resultado=parseInt(resultado);
+
+	terreno=(largo*2)+(ancho*2);
+	resultado=terreno*3;
+
+
+	alert(resultado);	
+
 
 }
