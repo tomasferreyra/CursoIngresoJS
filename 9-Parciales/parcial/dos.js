5c9c6db511c614963d5599af199434708d90b390
Tomás Iván Ferreyra Bussolo <ferreyrabussolotomas@gmail.com>
2018-02-09 21:41:50 -0300

    ejercicio 1 y 2

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,13 @@
 function Mostrar()
 {
-  
+	var importe;
+	var resulado;
+
+	importe=prompt("Precio");
+
+	importe=parseInt(importe);
+
+ 	resulado=((importe*21)/100)+importe;
+
+	document.getElementById('importeFinal').value=resulado;
 }
--- 9-Parciales/parcial/uno.js
+++ 9-Parciales/parcial/uno.js
@@ -1,5 +1,15 @@
 
 function Mostrar()
 {
+	var base;
+	var resulado;
+
+	base=document.getElementById('laBase').value;
+
+	resulado=parseInt(resulado);
+
+	resulado=base*4
+
+	alert(resulado);
 
 }
