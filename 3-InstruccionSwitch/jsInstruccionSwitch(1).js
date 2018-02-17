bf5cc19b7ab1e9907414dd2f06b40b6962e3cb47
Tomás Iván Ferreyra Bussolo <ferreyrabussolotomas@gmail.com>
2018-02-16 23:48:39 -0300

    Switch 1 y 2
    switch y default

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,9 +1,23 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mes;
 
+	mes=document.getElementById('mes').value;
 
-
-
+	switch(mes)
+	{
+		case "Enero":
+		alert("que comiences bien el año!!!.");
+		break;
+		case "Marzo":
+		alert("a clases!!!.");
+		break;
+		case "Julio":
+		alert("se vienen las vacaciones!!!.");
+		break;
+		case "Diciembre":
+		alert("Felices fiesta!!!.");
+		break;
+	}
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,11 +1,23 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mes;
 
-alert (mesDelAño);
+	mes=document.getElementById('mes').value;
 
-
-
-
+	switch(mes)
+	{
+		case "Julio":
+		case "Agosto":
+		alert("Abrigate que hace frio.");
+		break;
+		case "Septiembre":
+		case "Octubre":
+		case "Noviembre":
+		case "Diciembre":
+		alert("Ya pasamos el frio, ahora calor!!!.");
+		break;
+		default:
+		alert("Falta para el invierno.");
+	}
 }//FIN DE LA FUNCIÓN
