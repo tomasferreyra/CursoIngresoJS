0bc295abe44cb531653928df8478b221c7486f31
Tomás Iván Ferreyra Bussolo <ferreyrabussolotomas@gmail.com>
2018-02-23 21:55:04 -0300

    ej 3

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,9 @@
 function Mostrar()
 {
+	var repetciones = prompt("ingrese el número de repeticiones");
 
-var repetciones = prompt("ingrese el número de repeticiones");
-
-
-
+	for(i=0; i<repetciones; i++)
+	{
+		document.write("<br>Hola UTN FRA"+i);
+	}
 }//FIN DE LA FUNCIÓN
