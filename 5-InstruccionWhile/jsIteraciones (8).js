f1a9aa246fb3eed42d1b49f2daee01a35eb0e63d
Tomás Iván Ferreyra Bussolo <ferreyrabussolotomas@gmail.com>
2018-02-23 21:19:34 -0300

    ej 8 y 9

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,14 +1,29 @@
 function Mostrar()
 {
-
 	var contador=0;
 	var positivo=0;
-	var negativo=1;
-	
+	var negativo=1; //todo numero multplicado por 0 es 0
 	var respuesta='si';
+	var numero;
 
-
-document.getElementById('suma').value=positivo;
-document.getElementById('producto').value=negativo;
-
+	while(respuesta=="si")
+	{
+		contador++;
+		numero=prompt("ingrese numero");
+		numero=parseInt(numero);
+		if(numero>0)
+		{
+			positivo+=numero;
+		}
+		else
+		{
+			if(numero<0)
+			{
+				negativo*=numero;
+			}	
+		}
+		respuesta=prompt("para continuar, si");
+	}
+	document.getElementById('suma').value=positivo;
+	document.getElementById('producto').value=negativo;
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,39 @@
 function Mostrar()
 {
-
-	var contador=0;
+	var bandera=0;
 	// declarar variables
-	
 	var respuesta='si';
+	var maximo;
+	var minimo;
+	var numero;
 
 	while(respuesta!='no')
 	{
-		
-	
+		numero=prompt("ingrese numero");
+		numero=parseInt(numero);
+		respuesta=prompt("desea continuar");
+		if(bandera==0)
+		{
+			bandera++;
+			maximo=numero;
+			minimo=numero;
+		}
+		else
+		{
+			if(maximo<numero)
+			{
+
+				maximo=numero;
+			}
+			else
+			{
+				if(minimo>numero)
+				{
+					minimo=numero;
+				}
+			}
+		}
 	}
-
-
-
-
+	document.getElementById('maximo').value=maximo;
+	document.getElementById('minimo').value=minimo;
 }//FIN DE LA FUNCIÓN
