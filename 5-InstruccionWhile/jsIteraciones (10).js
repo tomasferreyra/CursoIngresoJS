72e8e9e48ba318ef3376dff6ffaf4ef33b8994cb
Tomás Iván Ferreyra Bussolo <ferreyrabussolotomas@gmail.com>
2018-02-23 21:21:19 -0300

    ej 10
    sin terminar

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -3,16 +3,46 @@ function Mostrar()
 
 	var contador=0;
 	//declarar contadores y variables 
-	
 	var respuesta="si";
-
+	var numero;
+	var negativo=0;
+	var positivo=0;
+	var cero=0;
+	var par=0;
+	var promedioP;
+	var promedioN;
+	var diferencia;
+	
 	while(respuesta!="no")
 	{
-		
-	
+		numero=prompt("ingrese numero");
+		numero=parseInt(numero);
+		if(numero<0)
+		{
+			contador++;
+			negativo+=numero;
+		}
+		else
+		{
+			if(numero>0)
+			{
+				contador++;
+				positivo+=numero;	
+			}
+			else
+			{
+				cero++;
+			}
+		}
+		if(numero%2==0 && numero!=0)
+		{
+			par++;
+		}
+		respuesta=prompt("desea continuar?");
 	}
-
-
-
+	document.write("Cantidad de positivos: "+positivo);
+	document.write("<br>Cantidad de negativos: "+negativo);
+	document.write("<br>Cantidad de ceros: "+cero);
+	document.write("<br>Cantidad de números pares: "+par);
 
 }//FIN DE LA FUNCIÓN
