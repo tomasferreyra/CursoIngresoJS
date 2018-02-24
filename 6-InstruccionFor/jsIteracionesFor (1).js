80b0c670031d456f7c8ed877c7ed66657efa9c52
Tomás Iván Ferreyra Bussolo <ferreyrabussolotomas@gmail.com>
2018-02-23 21:51:02 -0300

    ej 1 y 2

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,7 @@
 function Mostrar()
 {
-
+	for(i=1; i<11; i++)	
+	{
+		document.write("<br>"+i);
+	}
 }
--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,7 @@
 function Mostrar()
 {
-
-
-}
+	for(i=10; i>0; i--)	
+	{
+		document.write("<br>"+i);
+	}
+}		
